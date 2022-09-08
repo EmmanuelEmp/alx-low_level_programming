dab8fb1098f518f4c0d2924c087edb2d545843a2
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-08 11:34:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - Serves as the entry point for the program
  * Return: By default returns 0 if no errors otherwise returns non-0 value
  */
-int main(void) {
+int main(void) 
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
