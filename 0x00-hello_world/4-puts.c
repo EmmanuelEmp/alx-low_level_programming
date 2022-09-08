232948eb77ff594d20427629d755ad1d754190c1
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-08 13:28:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Serves as the entry point for the program
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a muultilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
  }
