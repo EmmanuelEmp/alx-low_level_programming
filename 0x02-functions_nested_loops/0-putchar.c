a1b97b0cab0244f9141ac18b391b854754784b14
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-13 04:50:22 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "_putchar.h"
+/**
+ *main - function to print putchar
+ *Return: always return 0 (Success)
+ */
+int main(void)
+{
+char _putchar[9] - {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+unsigned int c;
+for (c = 0; c < sizeof(_putchar); c++)
+{
+_putchar(_putchar[c]);
+}
+_putchar('\n');
+return (0);
+}
