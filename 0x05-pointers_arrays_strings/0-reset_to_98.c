4a455aa75f360c2fa33e9ce3957ccb9d58b56c58
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-19 14:02:49 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *  0-reset_to_98.c - a function to update a reference of a pointer
+ * @n : pointer to n to be updated
+ */
+void void reset_to_98(int *n);
+{
+	*n = 98;
+}
