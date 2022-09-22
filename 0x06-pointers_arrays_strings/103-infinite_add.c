2d41541a7f1827278fd6df60fd14671a58e01195
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-22 13:06:03 -0700

    Write a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,64 +5,24 @@
  * @n: integer params
  * Return: 0
  */
-* infinite_add - C function that adds two numbers stored
-*in strings to a buffer.
-*Assumes that strings are never empty and
-*that numbers will always be positive, or 0.
-*Assumes there are only digits stored in the number strings.
-*If result can be stored in the buffer,
-*returns a pointer to the result.
-*If result cannot be stored in the buffer, returns `0`.
-*@n1:first number to be added
-*@n2:second number to be added
-*@r: store result
-*@size_r: size of buffer
-*Return:returns pointer to result
-*/
 
 void rev_string(char *n)
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-}
-
-/**
-* add_strings - Adds the numbers stored in two strings.
-* @n1: The string containing the first number to be added.
-* @n2: The string containing the second number to be added.
-* @r: The buffer to store the result.
-* @r_index: The current index of the buffer.
-*
-* Return: If r can store the sum - a pointer to the result.
-*         If r cannot store the sum - 0.
-*/
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int i = 0;
 	int j = 0;
 	char temp;
-	int num, tens = 0;
 
 	while (*(n + i) != '\0')
-	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		i++;
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
 	}
 	i--;
 
 	for (j = 0; j < i; j++, i--)
-	for (; *n1; n1--; r_index++)
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
-		num = *(n1 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
 	}
 }
 
@@ -89,7 +49,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (j >= size_r || i >= size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
-	for (; *n2; n2--;  r_index--)
 	{
 		if (i < 0)
 			val1 = 0;
@@ -110,14 +69,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		digits++;
 		j--;
 		i--;
-		num = (*n2 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10; 
 	}
 	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
-
 }
