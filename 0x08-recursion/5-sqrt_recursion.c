e259900a4e412ba624551d54f6cfb4c90c33c614
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-28 16:35:40 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - find natural square root
- * @n: int
- * Return: int
  * power_operation - returns the natural square root of a number.
  * @n: input number.
  * @a: iterator.
  * Return: square root or -1
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * Return: int
  */
 
-int _sqrt_recursion(int n)
 int power_operation(int n, int a)
+int _sqrt_recursion(int n)
 {
-return (square(n, 1));
 	if (a % (n / a) == 0)
 	{
 		if (a * (n / a) == n)
@@ -22,21 +21,23 @@ return (square(n, 1));
 			return (-1);
 	}
 	return (0 + power_operation(n, a + 1));
+return (square(n, 1));
 }
 
 /**
+ * _sqrt_recursion - returns natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
  * square - find square root
  * @n: int to find square root
  * @val: square root
  * Return: int
- * _sqrt_recursion - returns natural square root of a number.
- * @n: input number.
- * Return: natural square root.
  */
 
 int square(int n, int val)
 {
 
+int _sqrt_recursion(int n)
 if (val * val == n)
 	return (val);
 else if (val * val < n)
@@ -44,13 +45,4 @@ else if (val * val < n)
 else
 	return (-1);
 
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (power_operation(n, 2));
 }
