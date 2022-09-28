27f65e16cae9265a6f94a0d1f763ab4e9c79e8d0
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-09-28 16:21:32 -0700

    Write a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,44 @@
-#include "main.h"
-
-/**
- * *_evalute - evaluate function sqrt.
- * @i: integer
- * @n: interger
- * Return: evaluate sqrt
- */
-int _evaluate(int i, int n)
+int _sqrt_recursion(int n)
+int power_operation(int n, int a)
 {
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-	else if (i * i == n) /*condition base*/
-		return (1);
-
-	return (i);
-
-	return (-1);
-
-	return (-1);
+return (square(n, 1));
+	if (a % (n / a) == 0)
+	{
+		if (a * (n / a) == n)
+			return (a);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, a + 1));
 }
 
 /**
- * *_sqrt_recursion - evaluate sqrt
- * @n: integer
- * Return: sqrt_recursion
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ * _sqrt_recursion - returns natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
  */
+
+int square(int n, int val)
+{
+
+if (val * val == n)
+	return (val);
+else if (val * val < n)
+	return  (square(n, val + 1));
+else
+	return (-1);
+
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
-	if (i < 0)
+	if (n < 0)
 		return (-1);
-
-	else
-	{
-		return (_evaluate(i, n)); /*recursive call*/
-	}
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
