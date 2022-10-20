b2202ec13cde63d691f90fee91ccdc1730fd483b
Ochogwu Emmanuel <emmyochogwu@gmail.com>
2022-10-20 15:29:03 -0700

    All

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - it is a function that free list_t
+ * @head: the first element
+ * Return: no
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
